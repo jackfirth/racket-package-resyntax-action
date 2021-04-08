06f46ddb9c0727c55a330b10a199a710a06d0173
Rebecca Turner <rbt@sent.as>
2021-04-08 16:00:41 -0400

    fix INPUT_DIRECTORY

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,8 @@ set -ev
 cd "$GITHUB_WORKSPACE"
 if [[ -z "$INPUT_NAME" ]]; then
     INPUT_NAME=$(basename "$GITHUB_REPOSITORY")
-    echo "INPUT_NAME is now $INPUT_NAME"
 fi
-echo "INPUT_NAME is now(2) $INPUT_NAME"
+INPUT_DIRECTORY=$(realpath "$PWD/$INPUT_DIRECTORY")
 
 raco pkg install --name "$INPUT_NAME" --batch --auto --link "$INPUT_DIRECTORY"
 racket -l racket-package-resyntax-action
