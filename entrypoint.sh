7dbea1c536ff57a4b26e00e2fcf0d518e838d113
Rebecca Turner <rbt@sent.as>
2021-04-08 15:49:27 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
-
 set -ev
-
 cd "$GITHUB_WORKSPACE"
-
-# Absolutely no idea why GitHub Actions seems convinced this package isn't
-# installed already by this point, but whatever.
-# raco pkg install --batch --auto https://github.com/jackfirth/resyntax.git
-
 if [[ -z "$INPUT_NAME" ]]; then
     INPUT_NAME=$(basename "$GITHUB_REPOSITORY")
+    echo "INPUT_NAME is now $INPUT_NAME"
 fi
+echo "INPUT_NAME is now(2) $INPUT_NAME"
 
 raco pkg install --name "$INPUT_NAME" --batch --auto --link "$INPUT_DIRECTORY"
 racket -l racket-package-resyntax-action
