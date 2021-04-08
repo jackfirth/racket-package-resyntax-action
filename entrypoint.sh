ef2df60f6c569b24b47a03c2fc46b496538285ff
Rebecca Turner <rbt@sent.as>
2021-04-08 15:39:15 -0400

    update script!!!!!!

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,9 @@ cd "$GITHUB_WORKSPACE"
 # installed already by this point, but whatever.
 # raco pkg install --batch --auto https://github.com/jackfirth/resyntax.git
 
+if [[ -z "$INPUT_NAME" ]]; then
+    INPUT_NAME=$(basename "$GITHUB_REPOSITORY")
+fi
+
 raco pkg install --name "$INPUT_NAME" --batch --auto --link "$INPUT_DIRECTORY"
 racket -l racket-package-resyntax-action
